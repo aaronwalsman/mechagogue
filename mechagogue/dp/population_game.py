6493cda2aaae111856ea25d08185d0bf5deac601
Aaron Walsman <aaronwalsman@gmail.com>
2025-01-22 15:33:07 -0500

    tweaking standards

--- mechagogue/dp/population_game.py
+++ mechagogue/dp/population_game.py
@@ -3,7 +3,7 @@ from typing import Any, Callable
 import jax.random as jrng
 
 def population_game(
-    params: Any,
+    config: Any,
     initialize_fn: Callable,
     transition_fn: Callable,
     observe_fn: Callable,
@@ -14,32 +14,32 @@ def population_game(
     Bundles the component functions of a population game into reset and step
     functions.  The components are:
     
-    params: configuration parameters that do not change during evaluation.
+    config: configuration parameters that do not change during evaluation.
     initialize_fn: a function which constructs a new environment state given
-        a random key and params
+        a random key and config
     transition_fn: a function which constructs a new environment state given
-        a random key, params, a previous state and actions for each player
+        a random key, config, a previous state and actions for each player
     observe_fn: a function which constructs observations for each player
-        given a random key, params and a state
-    players_fn: a function which lists the current players given params and
+        given a random key, config and a state
+    players_fn: a function which lists the current players given config and
         a state
     parents_fn: a function which lists the parents of the current players
-        given params and a state
+        given config and a state
     '''
     def reset(key):
         initialize_key, observe_key = jrng.split(key, 2)
-        state = initialize_fn(initialize_key, params)
-        players = players_fn(params, state)
-        parents = parents_fn(params, state)
-        obs = observe_fn(observe_key, params, state)
+        state = initialize_fn(initialize_key, config)
+        players = players_fn(config, state)
+        parents = parents_fn(config, state)
+        obs = observe_fn(observe_key, config, state)
         return state, obs, players, parents
     
     def step(key, state, action):
         transition_key, observe_key = jrng.split(key, 2)
-        next_state = transition_fn(transition_key, params, state, action)
-        players = players_fn(params, next_state)
-        parents = parents_fn(params, next_state)
-        obs = observe_fn(key, params, next_state)
+        next_state = transition_fn(transition_key, config, state, action)
+        players = players_fn(config, next_state)
+        parents = parents_fn(config, next_state)
+        obs = observe_fn(key, config, next_state)
         return next_state, obs, players, parents
     
     return reset, step
