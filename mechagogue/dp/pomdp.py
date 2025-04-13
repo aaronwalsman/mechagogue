1b3ca76f8a70f2956284b373b9d14e3508a225bb
Aaron Walsman <aaronwalsman@gmail.com>
2025-04-13 19:06:36 -0400

    updating pomdp/mdp

--- mechagogue/dp/pomdp.py
+++ mechagogue/dp/pomdp.py
@@ -9,45 +9,41 @@ import jax.random as jrng
 from mechagogue.arg_wrappers import ignore_unused_args
 
 def pomdp(
-    initialize_fn : Callable,
-    transition_fn : Callable,
-    observe_fn : Callable,
-    reward_fn : Callable = lambda : 0.,
-    done_fn : Callable = lambda : False,
-    config : Any = None,
+    init_state : Callable,
+    transition : Callable,
+    observe : Callable,
+    reward : Callable = lambda : 0.,
+    terminal : Callable = lambda : False,
 ) -> Tuple[Callable, Callable] :
     '''
     Builds reset and step functions for a partially observable markov decision
     process (POMDP) from its various components.
     
     [wikipedia](www.wikipedia.com/pomdp)
-    `test <www.google.com>`
     
     The components of a POMDP are:
     
-    config:
-        A static set of environment-specific configuration parameters defining
-        properties of the environment.
-    
-    initialize_fn(key, config) -> state:
+    init_state(key) -> state:
         A stochastic function that samples an initial state.
     
-    transition_fn(key, config, state, action) -> state:
+    transition(key, state, action) -> state:
         A stochastic function that maps a state and action to a new state.
     
-    observe_fn(key, config, state) -> obs:
+    observe(key, state) -> obs:
         A stochastic function that maps a state to an observation.
     
-    reward_fn(key, [state, action, next_state]) -> float:
+    reward(key, state, action, next_state) -> float:
         A stochastic function mapping some combination of state, action and
-        next state to reward.  The 'reward_format' argument controls which
-        of [state, action, next_state] should be used to compute the reward.
+        next state to reward.
     
-    done_fn(key, [state, action, next_state]) -> bool:
+    terminal(key, state, action, next_state) -> bool:
         A stochastic function mapping some combination of state, action and
         next state to a boolean indicating that the current episode has
-        terminated.  The 'done_format' argument controls which of
-        [state, action, next_state] should be used to compute done.
+        terminated.
+    
+    Any of the functions above may require only a subset of the listed
+        arguments.  The ignore_unused_args wrapper will filter out those
+        which are not required.
     
     The returned functions are:
     
@@ -59,39 +55,39 @@ def pomdp(
         state and action.
     '''
     
-    initialize_fn = ignore_unused_args(initialize_fn, ('key', 'config'))
-    transition_fn = ignore_unused_args(
-        transition_fn, ('key', 'config', 'state', 'action'))
-    observe_fn = ignore_unused_args(observe_fn, ('key', 'config', 'state'))
-    reward_fn = ignore_unused_args(
-        reward_fn, ('key', 'config', 'state', 'action', 'next_state'))
-    done_fn = ignore_unused_args(
-        done_fn, ('key', 'config', 'state', 'action', 'next_state'))
+    init_state = ignore_unused_args(
+        init_state, ('key',))
+    transition = ignore_unused_args(
+        transition, ('key', 'state', 'action'))
+    observe = ignore_unused_args(
+        observe, ('key', 'state'))
+    terminal = ignore_unused_args(
+        terminal, ('state',))
+    reward = ignore_unused_args(
+        reward, ('key', 'state', 'action', 'next_state'))
     
     def reset(key):
         # generate new keys
-        initialize_key, observe_key = jrng.split(key, 2)
+        state_key, observe_key = jrng.split(key)
         
         # generate the first state and observation
-        state = initialize_fn(initialize_key, config)
-        obs = observe_fn(observe_key, config, state)
+        state = init_state(state_key)
+        obs = observe(observe_key, state)
+        done = terminal(state)
         
         # return
-        return state, obs
+        return state, obs, done
     
     def step(key, state, action):
         # generate new keys
-        transition_key, observe_key, reward_key, done_key = jrng.split(key, 4)
+        transition_key, observe_key, done_key, reward_key = jrng.split(key, 4)
         
-        # generate the next state and observation
-        next_state = transition_fn(transition_key, config, state, action)
-        obs = observe_fn(observe_key, config, next_state)
-        
-        # compute the reward and done
-        reward = reward_fn(reward_key, config, state, action, next_state)
-        done = done_fn(done_key, config, state, action, next_state)
+        next_state = transition(transition_key, state, action)
+        obs = observe(observe_key, next_state)
+        done = terminal(next_state)
+        rew = reward(reward_key, state, action, next_state)
         
         # return
-        return next_state, obs, reward, done
+        return next_state, obs, done, rew
     
     return reset, step
