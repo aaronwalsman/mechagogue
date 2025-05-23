959f57bd57879b21d88f16ae931eb94892e1cc59
Aaron Walsman <aaronwalsman@gmail.com>
2025-05-22 21:33:32 -0400

    population game -> poeg

--- mechagogue/dp/poeg.py
+++ mechagogue/dp/poeg.py
@@ -4,7 +4,7 @@ import jax.random as jrng
 
 from mechagogue.arg_wrappers import ignore_unused_args
 
-def population_game(
+def poeg(
     init_state: Callable,
     transition: Callable,
     observe: Callable,
@@ -12,8 +12,8 @@ def population_game(
     family_info: Callable,
 ):
     '''
-    Bundles the component functions of a population game into reset and step
-    functions.  The components are:
+    Bundles the component functions of a partially observable ecological game
+    into reset and step functions.  The components are:
     
     init_state: a function which constructs a new environment state given
         a random key
