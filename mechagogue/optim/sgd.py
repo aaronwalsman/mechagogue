fae9a5159c0180d4b353d77acc302380db4641a6
Aaron Walsman <aaronwalsman@gmail.com>
2025-06-12 12:42:43 -0400

    updating optim to new structure

--- mechagogue/optim/sgd.py
+++ mechagogue/optim/sgd.py
@@ -1,6 +1,8 @@
 import jax
 import jax.numpy as jnp
 
+from mechagogue.static import static_functions
+
 def sgd(
     learning_rate=3e-4,
     momentum=0,
@@ -8,44 +10,46 @@ def sgd(
     nesterov=False,
     weight_decay=0,
 ):
-    def init(model_params):
-        def init_leaf(leaf):
+    @static_functions
+    class SGD:
+        def init(model_state):
+            def init_leaf(leaf):
+                if momentum:
+                    return jnp.zeros_like(leaf)
+                else:
+                    return None
+            return jax.tree.map(init_leaf, model_state)
+        
+        def optimize(grad, model_state, optim_state):
+            if weight_decay:
+                def leaf_weight_decay(leaf_grad, leaf_model_param):
+                    return leaf_grad + leaf_model_param * weight_decay
+                grad = jax.tree.map(leaf_weight_decay, grad, model_state)
+            
+            # TODO: is this right?
             if momentum:
-                return jnp.zeros_like(leaf)
+                def leaf_momentum(leaf_grad, leaf_param):
+                    return (leaf_param * momentum + leaf_grad * (1.-damping))
+                optim_state = jax.tree.map(leaf_momentum, grad, optim_state)
+            
+                velocity = optim_state
             else:
-                return None
-        return jax.tree.map(init_leaf, model_params)
+                velocity = grad
+            
+            if nesterov:
+                def leaf_nesterov(leaf_grad, leaf_velocity, leaf_param):
+                    return leaf_grad + leaf_velocity * momentum
+                model_update = jax.tree.map(
+                    leaf_nesterov, grad, velocity, optim_state)
+            else:
+                model_update = velocity
+            
+            def apply_leaf_update(leaf_model_param, leaf_update):
+                return leaf_model_param - leaf_update * learning_rate
+            
+            model_state = jax.tree.map(
+                apply_leaf_update, model_state, model_update)
+            
+            return model_state, optim_state
     
-    def optim(grad, model_params, optim_params):
-        if weight_decay:
-            def leaf_weight_decay(leaf_grad, leaf_model_param):
-                return leaf_grad + leaf_model_param * weight_decay
-            grad = jax.tree.map(leaf_weight_decay, grad, model_params)
-        
-        # TODO: is this right?
-        if momentum:
-            def leaf_momentum(leaf_grad, leaf_param):
-                return (leaf_param * momentum + leaf_grad * (1.-damping))
-            optim_params = jax.tree.map(leaf_momentum, grad, optim_params)
-        
-            velocity = optim_params
-        else:
-            velocity = grad
-        
-        if nesterov:
-            def leaf_nesterov(leaf_grad, leaf_velocity, leaf_param):
-                return leaf_grad + leaf_velocity * momentum
-            model_update = jax.tree.map(
-                leaf_nesterov, grad, velocity, optim_params)
-        else:
-            model_update = velocity
-        
-        def apply_leaf_update(leaf_model_param, leaf_update):
-            return leaf_model_param - leaf_update * learning_rate
-        
-        model_params = jax.tree.map(
-            apply_leaf_update, model_params, model_update)
-        
-        return model_params, optim_params
-    
-    return init, optim
+    return SGD
