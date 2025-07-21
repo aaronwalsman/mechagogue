8bc0213595b2a255050fccfb2fe55bd2c19a021b
Aaron Walsman <aaronwalsman@gmail.com>
2025-07-21 17:56:59 -0400

    epoch updates, better logging and loading

--- mechagogue/epoch.py
+++ mechagogue/epoch.py
@@ -1,3 +1,4 @@
+import time
 from typing import Any
 
 import jax
@@ -5,7 +6,7 @@ import jax.random as jrng
 
 from mechagogue.standardize import standardize_args
 from mechagogue.static import static_data, static_functions 
-from mechagogue.serial import save_leaf_data
+from mechagogue.serial import save_leaf_data, load_example_data
 from mechagogue.ds.system import (
     standardize_system, make_system, jit_system, iterated_system_scan)
 
@@ -22,6 +23,7 @@ def make_epoch_system(
     output_directory = '.',
     save_states = False,
     save_reports = False,
+    verbose = True,
 ):
     
     log = standardize_args(log, ('state', 'reports'))
@@ -55,16 +57,25 @@ def make_epoch_system(
     @static_functions
     class EpochSystem:
         
+        def state_path(epoch):
+            return f'{output_directory}/state_{epoch:08}.data'
+        
+        def reports_path(epoch):
+            return f'{output_directory}/reports_{epoch:08}.data'
+        
         def init(key):
             if system.init_has_aux:
                 system_state, *aux = system.init(key)
             else:
                 system_state = system.init(key)
             
-            return EpochState(system_state, 0)
+            return EpochState(system_state, 1)
         
         def step(key, state):
             epoch = state.epoch
+            if verbose:
+                print(f'epoch: {epoch}')
+                t_start = time.time()
             
             key, step_key = jrng.split(key)
             
@@ -89,29 +100,45 @@ def make_epoch_system(
                 length=steps_per_epoch,
             )
             
-            '''
-            system_state = system.step(key, state.system_state)
-            if make_report is not None:
-                system_state, reports = state
-            else:
-                reports = None
-            '''
             log(system_state, reports)
             state = EpochState(system_state, state.epoch+1)
             
-            if save_states:
-                state_path = f'{output_directory}/state_{epoch:08}.data'
+            if verbose:
                 state = jax.block_until_ready(state)
-                print(f'saving state to: {state_path}')
-                save_leaf_data(state, state_path)
+                t_primary = time.time()
+            
+            if save_states:
+                state_path = EpochSystem.state_path(epoch)
+                state = jax.block_until_ready(state)
+                if verbose:
+                    print(f'  saving state to: {state_path}')
+                save_leaf_data((key, state), state_path)
             
             if save_reports:
-                reports_path = (
-                    f'{output_directory}/reports_{epoch:08}.data')
+                reports_path = EpochSystem.reports_path(epoch)
                 reports = jax.block_until_ready(reports)
-                print(f'saving reports to: {reports_path}')
+                if verbose:
+                    print(f'  saving reports to: {reports_path}')
                 save_leaf_data(reports, reports_path)
             
+            if verbose:
+                state = jax.block_until_ready(state)
+                t_end = time.time()
+                primary_elapsed = t_primary -t_start
+                print(
+                    f'  {steps_per_epoch} steps took {primary_elapsed:.04}s '
+                    f'({steps_per_epoch/primary_elapsed:.4f}hz)'
+                )
+                secondary_elapsed = t_end - t_primary
+                print(f'  saving data took {secondary_elapsed:.4f}s')
+                total_elapsed = t_end - t_start
+                print(f'  total elapsed: {total_elapsed:.4f}')
+            
             return state
+        
+        def load_epoch(epoch):
+            state_path = EpochSystem.state_path(epoch)
+            example = jrng.key(0), EpochSystem.init(jrng.key(0))
+            return load_example_data(example, state_path)
     
     return EpochSystem
