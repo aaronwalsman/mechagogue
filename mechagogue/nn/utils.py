64c403bdec3d593167ef66dba5202e093e41a380
Aaron Walsman <awalsman@boslogin08.rc.fas.harvard.edu>
2025-08-19 15:53:47 -0400

    updating utils

--- mechagogue/nn/utils.py
+++ mechagogue/nn/utils.py
@@ -1,7 +1,11 @@
+import numpy as np
+
+import jax
+
 def num_parameters(model):
     def is_parameter(leaf):
         return hasattr(leaf, 'shape') and hasattr(leaf, 'dtype')
     def leaf_size(leaf):
         return int(np.prod(leaf.shape, dtype=np.int64))
-    leaves = jax.tree_util.tree_leaves(tree)
+    leaves = jax.tree_util.tree_leaves(model)
     return sum(leaf_size(leaf) for leaf in leaves if is_parameter(leaf))
